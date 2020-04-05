695c2f99017e92c0a53f09626c64e77505cbf5c8
Mel Garza <melgarza3016@gmail.com>
2020-04-04 20:43:26 -0500

    updated post route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,35 +8,35 @@ router.post("/api/workouts", (request, response) => {
   });
 });
 
-router.get("/api/workouts", (request, response) => {
-  //console.log('request', response);
-  Workout.find()
-    .then((dbWorkout) => {
-      response.json(dbWorkout);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+// router.get("/api/workouts", (request, response) => {
+//   //console.log('request', response);
+//   Workout.find()
+//     .then((dbWorkout) => {
+//       response.json(dbWorkout);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// });
 
-router.get("/api/workouts/range", (request, response) => {
-  Workout.find()
-    .sort({ day: -1 })
-    .then((dbWorkout) => {
-      response.json(dbWorkout);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+// router.get("/api/workouts/range", (request, response) => {
+//   Workout.find()
+//     .sort({ day: -1 })
+//     .then((dbWorkout) => {
+//       response.json(dbWorkout);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// });
 
-router.put("/api/workouts/:id", (request, response) => {
-  Workout.update(
-    { _id: request.params.id },
-    { $push: { exercises: request.body } }
-  ).then((dbWorkout) => {
-    response.json(dbWorkout);
-  });
-});
+// router.put("/api/workouts/:id", (request, response) => {
+//   Workout.update(
+//     { _id: request.params.id },
+//     { $push: { exercises: request.body } }
+//   ).then((dbWorkout) => {
+//     response.json(dbWorkout);
+//   });
+// });
 
 module.exports = router;
