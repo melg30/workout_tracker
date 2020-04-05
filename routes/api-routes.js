fc259d2081504cfc9659942b273f48e3f4ea435e
Mel Garza <melgarza3016@gmail.com>
2020-04-04 20:45:14 -0500

    last get and post updates

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -19,24 +19,24 @@ router.get("/api/workouts", (request, response) => {
     });
 });
 
-// router.get("/api/workouts/range", (request, response) => {
-//   Workout.find()
-//     .sort({ day: -1 })
-//     .then((dbWorkout) => {
-//       response.json(dbWorkout);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// });
+router.get("/api/workouts/range", (request, response) => {
+  Workout.find()
+    .sort({ day: -1 })
+    .then((dbWorkout) => {
+      response.json(dbWorkout);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
-// router.put("/api/workouts/:id", (request, response) => {
-//   Workout.update(
-//     { _id: request.params.id },
-//     { $push: { exercises: request.body } }
-//   ).then((dbWorkout) => {
-//     response.json(dbWorkout);
-//   });
-// });
+router.put("/api/workouts/:id", (request, response) => {
+  Workout.update(
+    { _id: request.params.id },
+    { $push: { exercises: request.body } }
+  ).then((dbWorkout) => {
+    response.json(dbWorkout);
+  });
+});
 
 module.exports = router;
